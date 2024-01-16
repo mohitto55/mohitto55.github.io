5ceee7184d53f681bb77d1ac51ff429765d12a00
mohitto55 <mohitto5865@gmail.com>
2024-01-16 19:40:45 +0900

    Add Font

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,17 @@
 var store = [{
-        "title": "깃허브로 이사",
+        "title": "[Git Blog]깃허브로 이사",
         "excerpt":"첫 깃허브 블로그 개설  원래는 티스토리에서 글을 작성하였다. 처음 입문하기에 더할나위 없는 블로그였고 잘 사용하고 있었으나 옵시디언을 내 입맛대로 꾸미다가 문득 블로그도 커스터마이징을 해서 꾸며보고 싶었졌다.  옵시디언을 하면서 마크다운 문법에 꽤나 익숙해져서 보다 자유도 높은 깃허브로 이사를 오게됐다.  ","categories": ["Git_Blog"],
         "tags": ["Blog"],
         "url": "/git_blog/%EA%B9%83%ED%97%88%EB%B8%8C%EB%A1%9C-%EC%9D%B4%EC%82%AC/",
         "teaser": null
       },{
-        "title": "Minimal Mistakes 사이드바 추가하기",
+        "title": "[Git Blog] invalid byte sequence in UTF-8 해결법",
+        "excerpt":"UTF-8 Error 열심히 유튜브를 보며 깃블로그 작성법을 공부하던 도중 bundle exec jeakll serve를 이용해 로컬서버를 돌리려는데 시작도 못하고 에러가 발생해 중지되었다. 에러 코드를 봤더니 invalid byte sequence in UTF-8 라고 나왔다. 검색을 통해 알아보니 파일 인코딩이 UTF-8이 아니라 다른 인코딩으로 되어있어서 발생하는 오류였다. 나는 Visual studio 2022 로 글을 작성...","categories": ["Git_Blog"],
+        "tags": ["Blog","Jekyll","MinimalMistake","UTF-8"],
+        "url": "/git_blog/UTF-8-ERROR/",
+        "teaser": null
+      },{
+        "title": "[Git Blog] Minimal Mistakes 사이드바 추가하기",
         "excerpt":"사이드바 Minimal-Mistakes 테마를 이용해 사이드바를 만들어 볼 것이다. 1. 사이드바 옵션 설정 사이드바를 만들기 전에 사이드바 내용을 정해줘야 한다. _data/navigation.yml 파일을 들어가자. _data 폴더는 블로그에서 사용할 데이터들을 모아 놓는 폴더이고 navigation.yml은 navigation에 필요한 데이터를 저장하는 파일이다. 파일에 들어가면 main 이라고 쓰인 오브젝트가 있을 것이다. 네비게이션에 쓰일 기본 데이터다. main: -...","categories": ["Git_Blog"],
         "tags": ["Blog","Jekyll","MinimalMistake"],
         "url": "/git_blog/%EC%82%AC%EC%9D%B4%EB%93%9C%EB%B0%94-%EC%B6%94%EA%B0%80/",
